4926bd7cf43eca50c123c501372f0252bf8b16cd
zohair <sabyoudzo@gmail.com>
2025-06-02 01:31:38 +0100

    DB for back end

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ import requests
 from datetime import datetime
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL', 'postgresql://localhost/exam_eye_db')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['UPLOAD_FOLDER'] = 'static'
 
